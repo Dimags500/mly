9d29d0ceb7119c326d42776684ee7800250fd3f1
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-06-22 16:40:30 +0300

    Update firebase.js

--- cinfig/firebase.js
+++ cinfig/firebase.js
@@ -8,12 +8,7 @@ import { getDatabase } from "firebase/database";
 
 export function StartFirebase() {
   const firebaseConfig = {
-    apiKey: "AIzaSyBZzjnTF3iAOp0Vh02oKmxQtJyFQWgwQPY",
-    authDomain: "mly-test.firebaseapp.com",
-    projectId: "mly-test",
-    storageBucket: "mly-test.appspot.com",
-    messagingSenderId: "512535345161",
-    appId: "1:512535345161:web:49ba741332a4e5dd14a0d8",
+  
   };
   const app = initializeApp(firebaseConfig);
   return getDatabase(app);
